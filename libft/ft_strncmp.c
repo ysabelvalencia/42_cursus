170d1c96ad77bcdb65f9c61b17492ca86134f5c5
Ysabel Valencia <ysavalen@student.42madrid.com>
2024-09-27 17:30:56 +0200

    update

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ysavalen <ysavalen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 14:29:32 by ysavalen          #+#    #+#             */
-/*   Updated: 2024/09/24 16:40:28 by ysavalen         ###   ########.fr       */
+/*   Updated: 2024/09/27 17:09:53 by ysavalen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (n == 0)
-		return (0);
-	if (*s1 != *s2)
-		return ((unsigned char) *s1 - (unsigned char) *s2);
-	if (n == 1)
-		return (0);
-	return (ft_strncmp(s1 + 1, s2 + 1, n - 1));
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
 }
