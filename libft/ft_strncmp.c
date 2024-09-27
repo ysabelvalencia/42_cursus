50d210fc0347eff50cde9e1deaf1556701d1eca4
ysabelvalencia <ysafvr8@gmail.com>
2024-09-28 00:26:15 +0200

    fixing errors

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -17,11 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	while (i < n && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
+	if (i < n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
