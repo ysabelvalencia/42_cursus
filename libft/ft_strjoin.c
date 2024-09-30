df6dd996db315095664199fad71d704c4725b834
ysabelvalencia <ysafvr8@gmail.com>
2024-10-01 00:23:12 +0200

    part 2, functions substr and strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysavalen <ysavalen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 23:59:22 by ysavalen          #+#    #+#             */
+/*   Updated: 2024/09/30 23:59:22 by ysavalen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new;
+
+	if (!s1 ||!s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = (char *)malloc(s1_len + s2_len + 1);
+	if (!new)
+		return (NULL);
+	ft_strlcpy(new, s1, s1_len + 1);
+	ft_strlcpy(new + s1_len, s2, s2_len + 1);
+	return (new);
+}
---
+++ libft/ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysavalen <ysavalen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 09:51:52 by ysavalen          #+#    #+#             */
+/*   Updated: 2024/09/30 09:51:52 by ysavalen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	s_len;
+
+	if (s == NULL)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+		str = malloc(1);
+		if (str == NULL)
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	if (len > s_len - start)
+		len = s_len - start;
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
+}
