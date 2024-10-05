474068674e78e59e13061dbc2c9f6b107da30725
Ysabel Valencia <ysavalen@c1r1s5.42madrid.com>
2024-10-05 18:03:19 +0200

    update

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,42 +6,66 @@
 /*   By: ysavalen <ysavalen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 20:37:55 by ysavalen          #+#    #+#             */
-/*   Updated: 2024/10/05 00:23:28 by ysavalen         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:03:08 by ysavalen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+static int	ft_count_substring(char const *s, char c)
+{
+	int	count;
+	int	in_substring;
+
+	count = 0;
+	in_substring = 0;
+	while (*s)
+	{
+		if (*s != c && !in_substring)
+		{
+			in_substring = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_subtring = 0;
+		s++;
+	}
+	return (count);
+}
+
+static char	*ft_malloc_word(char const *s, char c)
+{
+	char	*word;
+	int		len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	len = 0;
+	while (s[len] && s[len] != c)
+	{
+		word[len] = s[len];
+		len++;
+	}
+	word[len] = '\0';
+	return (word);
+}
+
+static void	free_split(char **split, int i)
+{
+	while (i > 0)
+	{
+		free (split[i - 1]);
+		i--;
+	}
+	free(split);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	size_t	s_len;
-	size_t	i;
-	size_t	start;
-	size_t	sub_index;
-	size_t	num_subtrings;
-	char	**result;
-	char	*substring;
-
-	if (s == NULL)
-		return (NULL);
-	s_len = ft_strlen(s);
-	i = 0;
-	while (i <= s_len)
-	{
-		if (s[i] != c && s[i] != '\0')
-		{
-			if (i = 0 || s[i - 1] == c)
-				start = i;
-		}
-		else
-		{
-			if (i > 0 && s[i - 1] != c)
-			{
-
-			}
-		}
-		i++;
-	}	
+	return (0);	
 }
-*/
