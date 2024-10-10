f76c95109039b0c28084577cc0412e27f6f033fa
ysabelvalencia <ysafvr8@gmail.com>
2024-10-10 22:09:31 +0200

    update

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
